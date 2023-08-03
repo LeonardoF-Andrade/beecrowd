c8515071c4f96da439d8d9a86d8cc2f28a48b481
Leonardo-FAnddrade <leonardo.andrade99@hotmail.com>
2023-08-03 14:40:57 -0300

    Version 3

--- Q-3129.py
+++ Q-3129.py
@@ -1,51 +1,48 @@
+R = 0
+D = 0
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
 class Lis:
+
     def ini(self):
         self.head = None
 
     def li(self, data):
+        global R, D
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
+            D+=1
         else:
             aux = self.head
-            while aux.next is not None:
-                aux = aux.next
-            aux.next = new_node
+            if data == aux.data:
+                R+=1
+            else:
+                while aux.next is not None:
+                    if aux.data == data:
+                        R+=1
+                    aux = aux.next
+                if aux.data == data:
+                    R+=1
+                else:
+                    aux.next = new_node
+                    D+=1
 
 n = int(input())
 
 lista = Lis()
 lista.ini()
 
-R = 0
-D = 0
-
-def analisa(lista, data):
-    global R, D
-    aux = lista.head
-    if aux.next is None:
-        D+=1
-    else:
-        aux = lista.head
-        cont = 0
-        while aux is not None:
-            if aux.data == data and aux.next is not None:
-                R+=1
-                cont+=1
-            elif aux.next is None and cont == 0:
-                D+=1
-            aux = aux.next
 
 
 for i in range(n):
     f = int(input())
     lista.li(f)
-    analisa(lista,f)
+
 
 print(D,"\n",R, sep="")
 

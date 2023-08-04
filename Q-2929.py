2043829cd765af60c907b778e2432c2021ae1486
Leonardo-FAnddrade <leonardo.andrade99@hotmail.com>
2023-08-03 23:45:03 -0300

    Version 2

--- Q-2929.py
+++ Q-2929.py
@@ -5,19 +5,23 @@ Val = int(input())
 def push(saco, grau):
     saco.append(grau)
     
-def MIN(saco):
-    print(min(saco))
-
 def POP(saco):
     saco.pop()
 
 for i in range(Val):
     A = input().split()
     if A[0] == "PUSH":
+        if len(saco) == 0:
+           mini = int(A[1])
+        else: 
+           if int(A[1]) < mini:
+              mini = int(A[1])
         push(saco,int(A[1]))
     elif len(saco) != 0:
         if A[0] == "MIN":
-         MIN(saco)
+         print(mini)
         elif A[0] == "POP":
          POP(saco)
+         if len(saco) != 0:
+          mini = min(saco)
     else: print("EMPTY")

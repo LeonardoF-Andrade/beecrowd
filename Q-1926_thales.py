5ac6c6d3fc6e142402a0de0046045ea0d5f8a443
Thales Paravela <thalesparavela@gmail.com>
2023-08-09 20:49:42 -0300

    teste 1926

--- Q-1926_thales.py
+++ Q-1926_thales.py
@@ -1,22 +1,34 @@
 casos = int(input())
-primos =[]
+primos =[2,3,5,7,11,13]
 twin = []
-def is_prime(n):
-    if n in primos:
+def is_prime(num):
+    if num in primos:
         return True
-    i = 2
-    while i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    if num < primos[-1]:
+        return False
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
             return False
-        else:
-            print(i)
-            ##primos.append()
         i += 6
-
+    primos.append(num)
     return True
+
+
 
 for caso in range(casos):
     primeiro, ultimo = (input().split())
     primeiro = int(primeiro)
     ultimo=int(ultimo)
-    is_prime(ultimo)
+    for numero in range(2, ultimo + 1):
+        if is_prime(numero):
+            indice = primos.index(numero)
+        else:
+            print(f"{numero} não é um número primo.")

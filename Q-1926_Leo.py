c785b22ca27ac93f95e21a4fcbd752e9e1a030b5
Leonardo-FAnddrade <leonardo.andrade99@hotmail.com>
2023-08-10 11:05:04 -0300

    Version 4

--- Q-1926_Leo.py
+++ Q-1926_Leo.py
@@ -1,82 +1,117 @@
 import sys
 
-def primo(val, p):
-    print(n)
-    if val in p:
+def primo(val, z, w, flag = True):
+    global Aux, cont
+    print("FFFFFF")
+    print(val, gemeos, primos, Aux)
+    if val in gemeos:
+        Aux += 1
+        return True
+    if val in pr:
         return True
     if val in n:
         return False
     if val <= 1:
         n.append(val)
         return False
-    if val <= 3:
-        p.append(val)
+    if val <= 3 and flag == False:
+       return True
+    if val <= 3 and flag:
+        if val == z:
+            if cont == 0:
+               print("AA")
+               if val <= z or val >= w:
+                Aux += 1
+               pr.append(val)
+               return True
+            if abs(val - proximoprimm(val)) == 2 :
+                if val not in gemeos:
+                 print("BB")
+                 gemeos.append(val)
+                 if val <= z or val >= w:
+                  Aux += 1
+            pr.append(val)
+            return True
+        elif cont == 0:
+         if abs(val - proximoprim(val)) == 2:
+            if val not in gemeos:
+                print("CC")
+                gemeos.append(val)
+                if val <= z or val >= w:
+                    Aux += 1
+        elif val < z:
+            if abs(val - proximoprim(val)) == 2:
+                 if val not in gemeos:
+                    print("DD")
+                    gemeos.append(val)
+                    if val <= z or val >= w:
+                     Aux += 1
+        pr.append(val)
         return True
     if val % 2 == 0 or val % 3 == 0:
-        n.append(val)
-        return False
+     n.append(val)
+     return False
     
     i = 5
     while i * i <= val:
         if val % i == 0 or val % (i + 2) == 0:
             n.append(val)
             return False
-        i += 6
-    
-    p.append(val)
-    return True
+        i += 6  
 
-def gemeo(vetor,cont, gem):
-    A = 0
-    cont2 = 0
-    for i in range (cont):
-        if vetor[i] in gem and cont2 == 0:
-            A +=1
-        elif i == (cont-1):
-            if abs(vetor[i]-vetor[i-1]) == 2:
-                if vetor[i] not in gem:
-                    gem.append(vetor[i])
-                    A+=1
-            if abs(vetor[i] - proximoprim(vetor[i])) == 2:
-                if vetor[i] not in gem:
-                    gem.append(vetor[i])
-                    A+=1
-        else:
-            cont2 = 0
-            if abs(vetor[i]-vetor[i+1]) == 2:
-                if vetor[i] not in gem:
-                    gem.append(vetor[i])
-                    A+=1
-                if vetor[i+1] not in gem:
-                    gem.append(vetor[i+1])
-                    cont2 +=1
-                    A+=1
-            elif abs(vetor[i]-proximoprimm(vetor[i])) == 2:
-                if vetor[i] not in gem:
-                    gem.append(vetor[i])
-                    A+=1                
-            
-    return A, gem
+    if val == z:
+        if cont == 0:
+            if val <= z or val >= w:
+             Aux += 1
+             print("EE")
+            pr.append(val)
+            return True
+        if abs(val - proximoprimm(val)) == 2 :
+            if val not in gemeos:
+                print("FF")
+                gemeos.append(val)
+                if val <= z or val >= w:
+                 Aux += 1
+        pr.append(val)
+        return True
+    elif cont == 0:
+        if abs(val - proximoprim(val)) == 2:
+         if val not in gemeos:
+            print("GG")
+            gemeos.append(val)
+            if val <= z or val >= w:
+                Aux += 1
+    elif val < z:
+        if abs(val - proximoprim(val)) == 2:
+                if val not in gemeos:
+                 print("HH")
+                 gemeos.append(val)
+                if val <= z or val >= w:
+                    Aux += 1
+    pr.append(val)
+    return True
 
 
 def proximoprim(A):
+    global X, Y 
     prox = A + 1
     while True:
-        if prox > A+2:
+        if prox > (A+2):
             return A
-        if primo(prox,pr):
+        if primo(prox,Y,X,False):
             return prox
         prox += 1
 
 def proximoprimm(A):
+    global X, Y
     prox = A - 1
     while True:
-        if prox < A-2:
+        if prox < (A-2):
             return A
-        if primo(prox,pr):
+        if primo(prox,Y,X,False):
             return prox
         prox -= 1
-        if prox <= 0:
+        if prox <= 1:
             break
     return A
 
@@ -84,14 +119,15 @@ gemeos = []
 pr = []
 n = []
 
+
 for i in range (0,int(input())):
     primos = []
+    Aux = 0
     cont = 0
     X, Y = map(int, sys.stdin.readline().strip().split())
     for i in range (X, Y+1):
-        if primo(i,pr):
+        if primo(i, Y, X):
             primos.append(i)
-            cont+=1
-    j , gemeos = gemeo(primos, cont, gemeos)
-    print(j)
+            cont +=1
+    print(Aux)
 
